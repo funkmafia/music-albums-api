570098437b64a0e260849dbf29932c557ef5313e
Zak <zak.hajjaji@icloud.com>
2025-05-21 14:51:25 +0100

    updated index file and package.json

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 require("dotenv").config(); // loads environment variables from a .env file into process
+
 const express = require("express");
 const mongoose = require("mongoose");
-const albumRoutes = require(""); // ./routes/albumRoutes
+const {
+  createAlbum,
+  getAllAlbums,
+  updateAlbums,
+  deleteAlbums,
+} = require("./controllers/albumControllers");
+
 const app = express();
-const PORT = process.env.PORT || 3000;
-const MONGODB_URL = process.env.MONGODB_URL;
-
-app.use(express.json()); // middleware
-app.use("/api/albums", albumRoutes);
-
 mongoose
   .connect(process.env.MONGODB_URL)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log("MongoDB connection error", err));
 
+app.use(express.json()); // middleware
+
 // GET request
-app.get("/", (request, response) => {
-  response.send("Hello MUSIC WORLD!");
+app.get("/api/albums", getAllAlbums);
+app.post("/api/albums", createAlbum);
+app.delete("/api/albums", deleteAlbums);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
